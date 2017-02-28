0430c7df7c639c938771a742dd017bfda970ff7a
Andy Yan <andyyan@Jins-MacBook-Pro.local>
2017-02-27 21:55:49 -0800

    Automated commit at 2017-02-28 05:55:49 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/nancy.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('div.buttons div.show').click(function() {
+    $('div.about').addClass('expanded');
+    $('div.about').removeClass('hide');
+  })
+  $('div.buttons div.hide').click(function() {
+    $('div.about').removeClass('expanded');
+    $('div.about').addClass('hide');
+  })
+})
+;
